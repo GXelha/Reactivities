9b4fb63b9ecbf1ad4b21eab2c2fa13d0a6982bb1
Juan Fassone <juan@xelsys.ca>
2019-11-28 17:17:37 -0500

    End of Section 5

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+
+interface IProps {
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  setSelectActivity: (activity: IActivity | null) => void;
+  selectedActivity: IActivity | null;
+  editMode: boolean;
+  setEditMode: (isEditMode: boolean) => void;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+  deleteActivity: (id: string) => void;
+}
+
+export const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  setEditMode,
+  editMode,
+  selectActivity,
+  setSelectActivity,
+  selectedActivity,
+  createActivity,
+  editActivity,
+  deleteActivity
+}) => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+        />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            editMode={editMode}
+            setSelectActivity={setSelectActivity}
+          />
+        )}
+        {editMode && (
+          <ActivityForm
+            key={(selectedActivity && selectedActivity.id) || 0}
+            activity={selectedActivity!}
+            setEditMode={setEditMode}
+            createActivity={createActivity}
+            editActivity={editActivity}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default ActivityDashboard;
